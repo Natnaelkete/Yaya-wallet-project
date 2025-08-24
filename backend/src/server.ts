ad2f14766528213a338ab9d0b5b23b9a499f62d0
natnaelkete <ketemanatnael023@gmail.com>
2025-08-24 12:47:30 +0300

    fix docker issue with prisma

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import hpp from "hpp";
+import transactionRoute from "./routes/transactionsRoute";
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(helmet());
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ limit: "1mb" }));
 app.use(hpp());
+
+app.use("/api", transactionRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on :${PORT}`);
